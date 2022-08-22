938e8786ce618f2e3c6fdedc1de8d7e721250931
Matthias Grieder <25645980+zeitvertrieb@users.noreply.github.com>
2022-08-22 09:53:20 +0200

    Update PicspaceLink.js

--- components/PicspaceLink.js
+++ components/PicspaceLink.js
@@ -5,7 +5,7 @@ export default function PicspaceLink() {
         href="http://picspace.io/"
         target="_blank"
         rel="noreferrer"
-        className="inline-flex items-center px-10 py-3 border border-transparent text-xl font-semibold rounded-full shadow-sm text-white bg-primary hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+        className="inline-flex items-center px-4 py-2 lg:px-8 lg:py-3 border border-transparent text-lg font-bold rounded-full shadow-sm text-white bg-primary hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
       >
         <span>Bilder suchen</span>
       </a>
