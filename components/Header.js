37ba0707d79a1fa7ece12a8be9af94c3293152cf
Matthias Grieder <25645980+zeitvertrieb@users.noreply.github.com>
2022-08-22 09:53:16 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Brand from '../components/Brand'
 
 export default function Header({ name }) {
   return (
-    <header className="flex justify-between w-full px-8 py-4 bg-gradient-to-b from-white dark:from-gray-900">
+    <header className="flex justify-between w-full px-4 py-4 lg:px-8 bg-gradient-to-b from-white dark:from-gray-900">
       <div className="w-40 h-auto block">
         <Link href="/">
           <a>
